f9ebfde4498b3501701415bea7abfa644a868539
desktop <desktop@DESKTOP-TQTS876>
2018-07-21 20:45:28 -0300

    Projeto: Desenvolvimento do Código e ajustes no Diagrama de Classe. Código: Classe Verificado. Diagrama: Diagrama de Classes.

--- src/sysmatricula/logica/Verificador.java
+++ src/sysmatricula/logica/Verificador.java
@@ -17,21 +17,20 @@ public class Verificador extends LerArquivo {
         System.out.println("matriculaSemDV ->> " + matriculaSemDV.toString());
         System.out.println("digitoVerificador ->> " + digitoVerificador.toString());
 
-        for(int i=0; i<=(matriculaSemDV.size()-1);i++) {
-            
-            if(matriculaSemDV.get(i).equals("")){
-            JOptionPane.showMessageDialog(null, "ERRO:\nHá um registro incoerente,\nNão há matricula, apenas dígito verificador (\" -"+matriculaSemDV.get(i).toString()+digitoVerificador.get(i)+" \"), talvez tenha sido registrado por equivoco na sintaxe,\n este será excluido da lista de verificação.\ncaso não queira continuar, atualize o arquivo e tente novamente\neste erro não irá interferir no processo, click OK para continuar e prosseguir.");
-           matriculaSemDV.remove(i);
-           digitoVerificador.remove(i);
-           
-          setMatriculaSemDV(matriculaSemDV);
-          setDigitoVerificador(digitoVerificador);
-          
-          System.out.println("==getMatriculaSemDV== >>> "+getMatriculaSemDV().toString());
-          System.out.println("==getDigitoVerificador== >>> "+getDigitoVerificador().toString());
+        for (int i = 0; i <= (matriculaSemDV.size() - 1); i++) {
+
+            if (matriculaSemDV.get(i).equals("")) {
+                JOptionPane.showMessageDialog(null, "ERRO:\nHá um registro incoerente,\nNão há matricula, apenas dígito verificador (\" -" + matriculaSemDV.get(i).toString() + digitoVerificador.get(i) + " \"), talvez tenha sido registrado por equivoco na sintaxe,\n este será excluido da lista de verificação.\ncaso não queira continuar, atualize o arquivo e tente novamente\neste erro não irá interferir no processo, click OK para continuar e prosseguir.");
+                matriculaSemDV.remove(i);
+                digitoVerificador.remove(i);
+
+                setMatriculaSemDV(matriculaSemDV);
+                setDigitoVerificador(digitoVerificador);
+
+                System.out.println("==getMatriculaSemDV== >>> " + getMatriculaSemDV().toString());
+                System.out.println("==getDigitoVerificador== >>> " + getDigitoVerificador().toString());
             }
-                   
-                   
+
         }
 
         if (matriculaSemDV.size() == digitoVerificador.size()) {
